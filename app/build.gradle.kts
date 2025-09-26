84cdab31bae531337c012584a9bb3b6c33e9b1e5
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2025-09-26 09:05:55 +0700

    Hoàn thành test SignUp/SignIn ở Local

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    // Thêm plugin KSP cho Kotlin Symbol Processing (thay thế kapt)
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3"
 }
 
 android {
@@ -34,8 +37,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
 }
 
@@ -48,5 +53,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("androidx.fragment:fragment-ktx:1.8.9")
+    implementation(libs.androidx.fragment.ktx)
+    // Room Runtime
+    implementation(libs.androidx.room.runtime)
+
+    // Kotlin Extensions và Coroutines cho Room
+    implementation(libs.androidx.room.ktx)
+
+    // KSP (Kotlin Symbol Processing) - Bộ xử lý chú thích
+    ksp(libs.androidx.room.compiler)
+
+    // Thư viện Test
+    testImplementation(libs.androidx.room.testing)
 }
