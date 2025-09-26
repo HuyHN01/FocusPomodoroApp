21b16677ce0216630130d0df0609d13f33ac08c2
Htu <lehuutu1207@gmail.com>
2025-09-26 09:45:47 +0700

    UI - to do list today

---
+++ app/src/main/java/com/example/focusmate/ui/pomodoro/TaskViewModel.kt
@@ -0,0 +1,25 @@
+
+package com.example.focusmate.ui.pomodoro
+
+import androidx.lifecycle.ViewModel
+import com.example.focusmate.data.repository.TaskRepository
+
+class TaskViewModel : ViewModel() {
+
+    val tasks = TaskRepository.tasks
+
+    fun addNewTask(title: String, pomodoroCount: Int) {
+        TaskRepository.addTask(title, pomodoroCount)
+    }
+    fun toggleTaskCompletion(taskId: String) {
+        TaskRepository.toggleTaskCompletion(taskId)
+    }
+
+    fun completeTask(taskId: String) {
+        TaskRepository.completeTask(taskId)
+    }
+
+    fun deleteTask(taskId: String) {
+        TaskRepository.deleteTask(taskId)
+    }
+}
