69f6a3919990efb518402d02d452d8fe63575766
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2025-10-27 13:40:30 +0700

    Tách class TimeState ra file riêng

--- app/src/main/java/com/example/focusmate/ui/pomodoro/PomodoroViewModel.kt
+++ app/src/main/java/com/example/focusmate/ui/pomodoro/PomodoroViewModel.kt
@@ -1,15 +1,9 @@
 package com.example.focusmate.ui.pomodoro
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.focusmate.data.repository.PomodoroRepository
 import com.example.focusmate.util.SoundEvent
-
-enum class TimerState {
-    IDLE, RUNNING, PAUSED,
-    BREAK_READY, BREAK_RUNNING, BREAK_PAUSED
-}
 
 class PomodoroViewModel : ViewModel() {
     val timeLeft: LiveData<Int> = PomodoroRepository.timeLeft
