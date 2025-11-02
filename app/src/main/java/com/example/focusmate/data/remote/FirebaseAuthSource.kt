1b31cac1fa3b9c041183ff6005b98e8fa790573d
HuyHN01 <167284534+HuyHN01@users.noreply.github.com>
2025-11-02 07:42:36 +0700

    Update & Tối ưu giao diện Auth

--- app/src/main/java/com/example/focusmate/data/remote/FirebaseAuthSource.kt
+++ app/src/main/java/com/example/focusmate/data/remote/FirebaseAuthSource.kt
@@ -4,8 +4,29 @@ package com.example.focusmate.data.remote
 
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class FirebaseAuthSource() {
     private val auth: FirebaseAuth = Firebase.auth
+
+    suspend fun register(email: String, password: String): FirebaseUser? =
+        suspendCoroutine { cont ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener { cont.resume(it.user) }
+                .addOnFailureListener { cont.resume(null) }
+        }
+
+    suspend fun login(email: String, password: String): FirebaseUser? =
+        suspendCoroutine { cont ->
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener { cont.resume(it.user) }
+                .addOnFailureListener { cont.resume(null) }
+        }
+
+    fun logout() = auth.signOut()
+
+    fun getCurrentUser(): FirebaseUser? = auth.currentUser
 }
